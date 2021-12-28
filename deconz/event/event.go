82a7b8ded014ebf0a25d9b20332d5d6e77045d56
Markus Fuchs <web-code@mfuchs.org>
2021-12-28 14:10:03 +0100

    added ZHABattery

--- deconz/event/event.go
+++ deconz/event/event.go
@@ -59,6 +59,11 @@ func (e *Event) ParseState(tl TypeLookuper) error {
 	}
 
 	switch t {
+	case "ZHABattery":
+		var s ZHABattery
+		err = json.Unmarshal(e.RawState, &s)
+		e.State = &s
+		break
 	case "ZHAFire":
 		var s ZHAFire
 		err = json.Unmarshal(e.RawState, &s)
---
+++ deconz/event/zhabattery.go
@@ -0,0 +1,14 @@
+package event
+
+// ZHABattery represents the battery state of a device
+type ZHABattery struct {
+	State
+	Battery int16
+}
+
+// Fields returns timeseries data for influxdb
+func (z *ZHABattery) Fields() map[string]interface{} {
+	return map[string]interface{}{
+		"battery": z.Battery,
+	}
+}
